3db4e0a68464a409fd0976f9e4e20e1555f4940e
David Nguyen <h250694@gmail.com>
2024-10-18 12:47:56 +0700

    Refactor image fetching logic to reuse existing camera data and improve performance

---
+++ camera.php
@@ -0,0 +1,49 @@
+<?php
+
+$camera = file_get_contents('camera.json');
+
+$cameras = json_decode($camera, true)['value'][1][0];
+
+
+
+// echo '<pre>';
+// print_r($cameras);
+// echo '</pre>';
+
+$id = [];
+
+foreach ($cameras as $key => $value) {
+    $camStatusIndex = array_search('UP', array_column($value['Properties'], 'Value', 'Name'));
+    $ManagementUnit = array_search('ManagementUnit', array_column($value['Properties'], 'Name', 'Value'));
+    if ($camStatusIndex === false) {
+        continue;
+    }
+
+    $id[] = [
+        'CamId' => $value['Properties'][0]['Value'],
+        'name' => $value['Properties'][1]['Value'],
+        'Disctrict' => ($value['Properties'][4]['Value'] === 'UP' ? 'N/A' : $value['Properties'][4]['Value']),
+        'SnapshotUrl' => $value['Properties'][2]['Value'],
+        'ManagementUnit' => $ManagementUnit,
+    ];
+
+    /*if ($value['Properties'][$camStatusIndex]['Value'] !== 'NOT_IMAGE') {
+        // $id[] = $value['Properties'][0]['Value'];
+        // $camStatusIndex = isset($value['Properties'][6]) && $value['Properties'][6]['Name'] === 'CamStatus' ? 6 : 4;
+        $id[$key][] = [
+            'id' => $value['Properties'][0]['Value'],
+            'name' => $value['Properties'][1]['Value'],
+            'Disctrict' => $value['Properties'][4]['Value'],
+            'url' => $value['Properties'][5]['Value'],
+            'status' => $value['Properties'][$camStatusIndex]['Value'],
+        ];
+        // $id[$key][] = [
+        //     'id' => $value['Properties'][0]['Value'],
+        //     'name' => $value['Properties'][1]['Value'],
+        //     'Disctrict' => $value['Properties'][4]['Value'],
+        //     'url' => $value['Properties'][5]['Value'],
+        // ];
+    }*/
+}
+header('Content-Type: application/json');
+echo json_encode($id);
